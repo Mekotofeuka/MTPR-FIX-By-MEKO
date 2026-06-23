b30e0da24883a1de64b571762a1010522976c64d
Mekotofeuka <137619369+Mekotofeuka@users.noreply.github.com>
2026-06-24 02:00:23 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -172,7 +172,7 @@ clear_screen() {
 show_header() {
     clear_screen
     echo ""
-    echo -e "  ${BOLD}Простой менеджер46 SYN FIX${NC}"
+    echo -e "  ${BOLD}Простой менеджер47 SYN FIX${NC}"
     echo -e "  ${DIM}===========================${NC}"
     echo ""
 
